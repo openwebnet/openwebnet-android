2efd7106c91a40dd0e1084d453d10dbbad7d77f8
Nicolas Moreau <moreau-nicolas@users.noreply.github.com>
2016-02-26 23:04:45 +0100

    Add automation feature
    Fixes openwebnet/openwenet-android#5

--- app/src/main/java/com/github/openwebnet/component/module/RepositoryModule.java
+++ app/src/main/java/com/github/openwebnet/component/module/RepositoryModule.java
@@ -1,10 +1,12 @@
 package com.github.openwebnet.component.module;
 
 import com.github.openwebnet.database.DatabaseRealm;
+import com.github.openwebnet.repository.AutomationRepository;
 import com.github.openwebnet.repository.DeviceRepository;
 import com.github.openwebnet.repository.EnvironmentRepository;
 import com.github.openwebnet.repository.GatewayRepository;
 import com.github.openwebnet.repository.LightRepository;
+import com.github.openwebnet.repository.impl.AutomationRepositoryImpl;
 import com.github.openwebnet.repository.impl.DeviceRepositoryImpl;
 import com.github.openwebnet.repository.impl.EnvironmentRepositoryImpl;
 import com.github.openwebnet.repository.impl.GatewayRepositoryImpl;
@@ -40,6 +42,12 @@ public class RepositoryModule {
     @Singleton
     LightRepository provideLight() {
         return new LightRepositoryImpl();
+    }
+
+    @Provides
+    @Singleton
+    AutomationRepository provideAutomation() {
+        return new AutomationRepositoryImpl();
     }
 
     @Provides
---
+++ app/src/main/java/com/github/openwebnet/service/impl/AutomationServiceImpl.java
@@ -0,0 +1,141 @@
+package com.github.openwebnet.service.impl;
+
+import com.github.niqdev.openwebnet.OpenSession;
+import com.github.niqdev.openwebnet.message.Automation;
+import com.github.openwebnet.component.Injector;
+import com.github.openwebnet.model.AutomationModel;
+import com.github.openwebnet.model.AutomationModel.Status;
+import com.github.openwebnet.repository.AutomationRepository;
+import com.github.openwebnet.service.AutomationService;
+import com.github.openwebnet.service.CommonService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.functions.Func2;
+import rx.schedulers.Schedulers;
+
+import static com.github.openwebnet.model.AutomationModel.Status.STOP;
+import static com.github.openwebnet.model.AutomationModel.Status.UP;
+import static com.github.openwebnet.model.AutomationModel.Status.DOWN;
+
+public class AutomationServiceImpl implements AutomationService {
+
+    private static final Logger log = LoggerFactory.getLogger(AutomationService.class);
+
+    @Inject
+    AutomationRepository automationRepository;
+
+    @Inject
+    CommonService commonService;
+
+    public AutomationServiceImpl() {
+        Injector.getApplicationComponent().inject(this);
+    }
+
+    @Override
+    public Observable<String> add(AutomationModel automation) {
+        return automationRepository.add(automation);
+    }
+
+    @Override
+    public Observable<Void> update(AutomationModel automation) {
+        return automationRepository.update(automation);
+    }
+
+    @Override
+    public Observable<Void> delete(String uuid) {
+        return automationRepository.delete(uuid);
+    }
+
+    @Override
+    public Observable<AutomationModel> findById(String uuid) {
+        return automationRepository.findById(uuid);
+    }
+
+    @Override
+    public Observable<List<AutomationModel>> findByEnvironment(Integer id) {
+        return automationRepository.findByEnvironment(id);
+    }
+
+    @Override
+    public Observable<List<AutomationModel>> findFavourites() {
+        return automationRepository.findFavourites();
+    }
+
+    private Func1<String, Automation> requestStatus = Automation::requestStatus;
+
+    private Func2<OpenSession, AutomationModel, AutomationModel> handleStatus = (openSession, automation) -> {
+        Automation.handleStatus(() -> automation.setStatus(STOP), () -> automation.setStatus(UP), () -> automation.setStatus(DOWN)).call(openSession);
+        return automation;
+    };
+
+    @Override
+    public Observable<List<AutomationModel>> requestByEnvironment(Integer id) {
+        return findByEnvironment(id)
+            .flatMapIterable(lightModels -> lightModels)
+            .flatMap(requestAutomation(requestStatus, handleStatus))
+            .collect(ArrayList::new, List::add);
+    }
+
+    @Override
+    public Observable<List<AutomationModel>> requestFavourites() {
+        return findFavourites()
+            .flatMapIterable(lightModels -> lightModels)
+            .flatMap(requestAutomation(requestStatus, handleStatus))
+            .collect(ArrayList::new, List::add);
+    }
+
+    private Func2<OpenSession, AutomationModel, AutomationModel> handleResponse(Status status) {
+        return (openSession, automation) -> {
+            Automation.handleResponse(() ->
+                automation.setStatus(status), () -> automation.setStatus(null)).call(openSession);
+            return automation;
+        };
+    }
+
+    @Override
+    public Observable<AutomationModel> stop(AutomationModel automation) {
+        Func1<String, Automation> requestStop = Automation::requestStop;
+
+        return Observable.just(automation).flatMap(requestAutomation(requestStop, handleResponse(STOP)));
+    }
+
+    @Override
+    public Observable<AutomationModel> moveUp(AutomationModel automation) {
+        Func1<String, Automation> requestMoveUp = Automation::requestMoveUp;
+
+        return Observable.just(automation).flatMap(requestAutomation(requestMoveUp, handleResponse(UP)));
+    }
+
+    @Override
+    public Observable<AutomationModel> moveDown(AutomationModel automation) {
+        Func1<String, Automation> requestMoveDown = Automation::requestMoveDown;
+
+        return Observable.just(automation).flatMap(requestAutomation(requestMoveDown, handleResponse(DOWN)));
+    }
+
+    private Func1<AutomationModel, Observable<AutomationModel>> requestAutomation(
+        Func1<String, Automation> request, Func2<OpenSession, AutomationModel, AutomationModel> handler) {
+        // TODO improvement: group by gateway and for each gateway send all requests together
+        return automation -> commonService.findClient(automation.getGatewayUuid())
+            .send(request.call(automation.getWhere()))
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .map(openSession -> handler.call(openSession, automation))
+            .onErrorReturn(throwable -> {
+                log.warn("automation={} | failing request={}", automation.getUuid(), request.call(automation.getWhere()).getValue());
+                // unreadable status
+                return automation;
+            });
+    }
+
+}
