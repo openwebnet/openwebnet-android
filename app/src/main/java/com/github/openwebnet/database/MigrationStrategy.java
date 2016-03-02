a867ce69ebd9d14d6ec626d805f75bd7eab24757
niqdev <niqdev@gmail.com>
2016-03-02 14:46:52 +0100

    fix realm migration strategy v2

--- app/src/main/java/com/github/openwebnet/database/MigrationStrategy.java
+++ app/src/main/java/com/github/openwebnet/database/MigrationStrategy.java
@@ -1,6 +1,7 @@
 package com.github.openwebnet.database;
 
 import io.realm.DynamicRealm;
+import io.realm.FieldAttribute;
 import io.realm.RealmMigration;
 import io.realm.RealmSchema;
 
@@ -22,13 +23,13 @@ public class MigrationStrategy implements RealmMigration {
         // Migrate to version 2: Add the AutomationModel class.
         if (oldVersion == 1) {
             schema.create("AutomationModel")
-                    .addField(FIELD_UUID, String.class)
-                    .addField(FIELD_ENVIRONMENT_ID, Integer.class)
-                    .addField(FIELD_GATEWAY_UUID, String.class)
-                    .addField(FIELD_NAME, String.class)
-                    .addField(FIELD_WHERE, String.class)
-                    .addField(FIELD_FAVOURITE, boolean.class)
-                    ;
+                .addField(FIELD_UUID, String.class, FieldAttribute.PRIMARY_KEY)
+                .addField(FIELD_ENVIRONMENT_ID, Integer.class, FieldAttribute.REQUIRED)
+                .addField(FIELD_GATEWAY_UUID, String.class, FieldAttribute.REQUIRED)
+                .addField(FIELD_NAME, String.class, FieldAttribute.REQUIRED)
+                .addField(FIELD_WHERE, String.class, FieldAttribute.REQUIRED)
+                .addField(FIELD_FAVOURITE, boolean.class);
+
             ++oldVersion;
         }
     }
