c050efda9d5e9a8c37160c6319513547ccb45cbb
Nicolas Moreau <moreau-nicolas@users.noreply.github.com>
2016-03-09 21:09:34 +0100

    Let custom widgets inherit from AppCompat widgets
    AppCompat widgets seems to be consistent across API levels.
    Fixes openwebnet/openwebnet-android#28.

--- app/src/main/java/com/github/openwebnet/view/custom/TextViewCustom.java
+++ app/src/main/java/com/github/openwebnet/view/custom/TextViewCustom.java
@@ -1,12 +1,13 @@
 package com.github.openwebnet.view.custom;
 
 import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 import com.github.openwebnet.R;
 
-public class TextViewCustom extends TextView {
+public class TextViewCustom extends AppCompatTextView {
 
     public TextViewCustom(Context context, AttributeSet attrs) {
         super(context, attrs);
