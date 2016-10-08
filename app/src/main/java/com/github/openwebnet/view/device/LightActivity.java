1964659b94bfda19b6b961da78570c866e419241
niqdev <niqdev@gmail.com>
2016-10-08 12:30:09 +0100

    light: update activity

--- app/src/main/java/com/github/openwebnet/view/device/LightActivity.java
+++ app/src/main/java/com/github/openwebnet/view/device/LightActivity.java
@@ -1,22 +1,32 @@
 package com.github.openwebnet.view.device;
 
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.github.niqdev.openwebnet.message.Lighting;
 import com.github.openwebnet.R;
 import com.github.openwebnet.component.Injector;
 import com.github.openwebnet.model.LightModel;
 import com.github.openwebnet.model.RealmModel;
 import com.github.openwebnet.service.LightService;
+import com.google.common.collect.Lists;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import rx.functions.Action1;
 
 public class LightActivity extends AbstractDeviceActivity {
 
@@ -31,10 +41,31 @@ public class LightActivity extends AbstractDeviceActivity {
     @BindView(R.id.editTextLightWhere)
     EditText editTextLightWhere;
 
+    @BindView(R.id.textViewLightPrefix)
+    TextView textViewLightPrefix;
+
+    @BindView(R.id.textViewLightSuffix)
+    TextView textViewLightSuffix;
+
+    @BindView(R.id.textViewLightInfo)
+    TextView textViewLightInfo;
+
     @BindView(R.id.checkBoxLightDimmer)
     CheckBox checkBoxLightDimmer;
 
+    @BindView(R.id.spinnerLightType)
+    Spinner spinnerLightType;
+
+    private SparseArray<Lighting.Type> lightTypeArray;
+
     private String lightUuid;
+
+    /*
+     * unfortunately there is no better way to prevent
+     * editTextLightWhere to be initialized to ""
+     * the first time when initEditLight is invoked
+     */
+    private boolean initLightTypeFirstTime = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +78,77 @@ public class LightActivity extends AbstractDeviceActivity {
 
         initSpinnerEnvironment();
         initSpinnerGateway();
+        initSpinnerLightType();
         initEditLight();
+    }
+
+    private void initSpinnerLightType() {
+        lightTypeArray = initSparseArray(Lists.newArrayList(
+            Lighting.Type.GENERAL, Lighting.Type.AREA,
+            Lighting.Type.GROUP, Lighting.Type.POINT_TO_POINT
+        ));
+
+        List<String> lightTypeLabels = Lists.newArrayList(
+            getString(R.string.light_label_general), getString(R.string.light_label_area),
+            getString(R.string.light_label_group), getString(R.string.light_label_point_to_point)
+        );
+        initSpinnerAdapter(spinnerLightType, lightTypeLabels);
+
+        spinnerLightType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                initLightType(lightTypeArray.get(spinnerLightType.getSelectedItemPosition()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {}
+        });
+    }
+
+    private void initLightType(Lighting.Type type) {
+        Action1<Integer> initView = visibility -> {
+            editTextLightWhere.setVisibility(visibility);
+            textViewLightSuffix.setVisibility(visibility);
+            textViewLightInfo.setVisibility(visibility);
+        };
+
+        Action1<String> initWhereValue = value -> {
+            // initialize all subsequent calls except the first time
+            if (!initLightTypeFirstTime) {
+                editTextLightWhere.setText(value);
+            }
+            // from now on every time reset editTextLightWhere
+            initLightTypeFirstTime = false;
+        };
+
+        switch (type) {
+            case GENERAL:
+                textViewLightPrefix.setText(getString(R.string.light_value_general));
+                initView.call(View.GONE);
+                initWhereValue.call(Lighting.WHERE_GENERAL_VALUE);
+                break;
+            case AREA:
+                textViewLightPrefix.setText(getString(R.string.light_prefix_default));
+                textViewLightInfo.setText(getString(R.string.light_info_area));
+                initView.call(View.VISIBLE);
+                initWhereValue.call("");
+                break;
+            case GROUP:
+                textViewLightPrefix.setText(getString(R.string.light_prefix_group));
+                textViewLightInfo.setText(getString(R.string.light_info_group));
+                initView.call(View.VISIBLE);
+                initWhereValue.call("");
+                break;
+            case POINT_TO_POINT:
+                textViewLightPrefix.setText(getString(R.string.light_prefix_default));
+                textViewLightInfo.setText(getString(R.string.light_info_point_to_point));
+                initView.call(View.VISIBLE);
+                initWhereValue.call("");
+                break;
+            default: {
+                throw new IllegalArgumentException("invalid lighting type");
+            }
+        }
     }
 
     private void initEditLight() {
@@ -55,6 +156,8 @@ public class LightActivity extends AbstractDeviceActivity {
         log.debug("initEditLight: {}", lightUuid);
         if (lightUuid != null) {
             lightService.findById(lightUuid).subscribe(light -> {
+                spinnerLightType.setSelection(findSelectedItem(lightTypeArray).apply(light.getLightingType()));
+
                 editTextLightName.setText(String.valueOf(light.getName()));
                 editTextLightWhere.setText(String.valueOf(light.getWhere()));
 
@@ -66,11 +169,16 @@ public class LightActivity extends AbstractDeviceActivity {
         }
     }
 
+    protected Lighting.Type getSelectedLightType() {
+        return lightTypeArray.get(spinnerLightType.getSelectedItemPosition());
+    }
+
     @Override
     protected void onMenuSave() {
         log.debug("name: {}", editTextLightName.getText());
         log.debug("where: {}", editTextLightWhere.getText());
         log.debug("dimmer: {}", checkBoxLightDimmer.isChecked());
+        log.debug("type: {}", getSelectedLightType());
         log.debug("environment: {}", getSelectedEnvironment());
         log.debug("gateway: {}", getSelectedGateway());
         log.debug("favourite: {}", isFavourite());
@@ -89,8 +197,13 @@ public class LightActivity extends AbstractDeviceActivity {
     private boolean isValidLight() {
         return isValidRequired(editTextLightName) &&
             isValidRequired(editTextLightWhere) &&
+            isValidLightType() &&
             isValidDeviceEnvironment() &&
             isValidDeviceGateway();
+    }
+
+    protected boolean isValidLightType() {
+        return isValidRequired((TextView) spinnerLightType.getSelectedView());
     }
 
     private LightModel parseLight() {
@@ -98,6 +211,7 @@ public class LightActivity extends AbstractDeviceActivity {
             .name(utilityService.sanitizedText(editTextLightName))
             .where(editTextLightWhere.getText().toString())
             .dimmer(checkBoxLightDimmer.isChecked())
+            .type(getSelectedLightType())
             .environment(getSelectedEnvironment().getId())
             .gateway(getSelectedGateway().getUuid())
             .favourite(isFavourite())
